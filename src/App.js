9d3d6db5e0a19ed4502f76eb613c0c25d2a16bb3
roni12g@gmail.com <roni12g@gmail.com>
2022-11-28 20:50:22 +0200

    ....

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+import React, { useState } from "react";
+import Home from "./Home";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login/login";
+import About from './About/About';
+import './App.scss';
 
-function App() {
+const App = () => {
+  const [isLogedIn, setIsLogedIn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-page">
+      <div className="container-wrap">
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/home" index element={<Home />} />
+            <Route path="login" index element={<Login />} />
+            <Route path="about" index element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
     </div>
+
   );
-}
+};
 
 export default App;
